1293d77143721c57ebf3c7658cac590a5f44b667
FNitzsche <44500210+FNitzsche@users.noreply.github.com>
2020-07-28 02:15:18 +0200

    neue Formel
    zu viel Blau

--- src/main/java/Gpu/ValueKernel.java
+++ src/main/java/Gpu/ValueKernel.java
@@ -35,15 +35,19 @@ public class ValueKernel extends Kernel {
         float dx = 0;
         float dy = 0;
         for (int i = 0; i < countB; i++) {
-            //v += (float)(1/(Math.pow(balls[i*3] - x, 2)) +(Math.pow(balls[i*3+1] - y, 2)))*balls[i*3+2];
-            v += (float)(1/(Math.abs(Math.pow(balls[i*3] - x, 2)) + Math.abs(Math.pow(balls[i*3+1] - y, 2))))*balls[i*3+2];
-            dx += (float)(2*(balls[i*3] - x)/Math.pow((Math.pow(balls[i*3] - x, 2)) +(Math.pow(balls[i*3+1] - y, 2)), 2))*balls[i*3+2];
-            dy += (float)(2*(balls[i*3+1] - y)/Math.pow((Math.pow(balls[i*3] - x, 2)) +(Math.pow(balls[i*3+1] - y, 2)), 2))*balls[i*3+2];
+            float factor = 1;
+            float distS = (float)(Math.pow(balls[i*3]-x, 2) + Math.pow(balls[i*3+1]-y, 2));
+            if (distS > 1){
+                factor = 0;
+            }
+            v += Math.pow(1 - Math.min(1, distS), 2)*balls[i*3+2]*factor;
+            dx += (float)(4 * (balls[i*3] - x) * (-Math.pow((balls[i*3] - x), 2) -Math.pow(balls[i*3+1] - y, 2) + 1))*factor*balls[i*3+2];
+            dy += (float)(4* (balls[i*3+1] - y) * (-Math.pow((balls[i*3+1] - y), 2) -Math.pow(balls[i*3] - x, 2) + 1))*factor*balls[i*3+2];
         }
-        float steep = 1-(float)(Math.sqrt(Math.pow(dx, 2f)+Math.pow(dy, 2f)))*0.01f;
+        float steep = 1-(float)(Math.sqrt(Math.pow(dx, 2f)+Math.pow(dy, 2f)));
         steep = Math.max(0, Math.min(1, steep));
-        values[gid*3] = Math.max(0, Math.min(1, v*0.2f+steep));
-        values[gid*3+1] = steep;
+        values[gid*3] = Math.max(0, Math.min(1, v));
+        values[gid*3+1] = Math.max(0, Math.min(1, -v));
         values[gid*3+2] = steep;
     }
 
